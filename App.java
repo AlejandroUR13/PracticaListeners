a3278f7cdfa45159bda4fc82ab21dd1b8231c20f
Alejandro Ulloa <144401093+AlejandroUR13@users.noreply.github.com>
2024-10-21 11:09:05 -0600

    Add files via upload

---
+++ App.java
@@ -0,0 +1,63 @@
+
+//import Introduccion.Credencial;
+//import Introduccion.Estudiante;
+//import Introduccion.Fecha;
+//import Introduccion.GrupoA;
+
+import GUI.JFramaeLOL;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        JFramaeLOL aux = new JFramaeLOL();
+
+        // NuevoFrame frame = new NuevoFrame("Nuevo Frame", 400, 200);
+
+        // JFrame f = new JFrame();
+        // f.setTitle(Titulo);
+        // f.setSize(100, 100);
+        // f.setVisible(true);
+        // f.setDefaultCloseOperation(0);
+
+        // Investigar que son los eventos
+
+        // Fecha f1 = new Fecha(12, 2, 2024);
+        // Fecha f2 = new Fecha(30, 12, 2030);
+        // Credencial aux = new Credencial("Juan Alberto", "Sistemas", 2020670089, f1,
+        // f2, "UPIIZ");
+        // Estudiante juan = new Estudiante(aux);
+        // Fecha fecha = new Fecha(2, 3, 1994);
+        // System.out.println(fecha.getDia());
+        // System.out.println(fecha.getMes());
+        // System.out.println(fecha.getAnio());
+
+        // fecha.setAnio(2000);
+
+        // System.out.println(fecha);
+        // private LinkedList<Estudiante> estudiantes;
+        /*
+         * Credencial credePablo = newCredencial("Pablo","sistemas", 2022670235, new
+         * Fecha(12,12,2024),"UPIIZ");
+         * Estudiante Perla = new Estudiante();
+         * Estudiante Pedro = new Estudiante();
+         * Estudiante Pablo = new Estudiante(credePablo);
+         * Estudiante Ernesto = new Estudiante();
+         * Estudiante Ñorda = new Estudiante();
+         */
+
+        /*
+         * GrupoA gpo_5cm1 = new GrupoA(5,"Joaquin", "matematicas", "8:30","Ñorda" )
+         * gpo_5cm1.agregarEstudiante(Ñorda);
+         * gpo_5cm1.agregarEstudiante(Ernesto);
+         * gpo_5cm1.agregarEstudiante(Pablo);
+         * gpo_5cm1.agregarEstudiante(Pedro);
+         * gpo_5cm1.agregarEstudiante(Perla);
+         * gpo_5cm1.mostrarEstudiantes();
+         */
+
+        // Perla.getCredencial().setBoleta(2022670235);
+        // Perla.getCredencial().getFecha_vigencia.setAnio(2030);
+    }
+    // hacer una clase de tipo grupo en donde identificaremos atributos de una
+    // coleccion de estudiantes
+
+}
---
+++ Bobbypulido.java
@@ -0,0 +1,18 @@
+package GUI;
+
+public class Bobbypulido implements ActionListener {
+    private Practicalistn frame;
+
+    public Bobbypulido (Practicalistn frame)
+    this.frame = frame;
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        for (int i = 0; i boton1.length; i++){
+            if (e.getSource() == boton1){
+contador1.setText(String.valeOF(contador1));
+            }
+        }
+
+    }
+}
